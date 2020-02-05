cef03263236e505e288fc063dd639584a9add548
automatedSequencing <35260345+automatedSequencing@users.noreply.github.com>
2020-02-05 16:37:05 +0100

    change variable naming

--- libBASE/pathconfig.py
+++ libBASE/pathconfig.py
@@ -4,14 +4,14 @@ import os
 
 # copy internal data directory to current location
 if(platform.system()=="Linux"):
-    IGDATA="/home/momsen/AGPruess/igblast/internal_data"
-    auxiliary_data=os.path.join('igblast','optional_file','human_gl.aux')
+    igblast_internal_data="/home/momsen/AGPruess/igblast/internal_data"
+    igblast_auxiliary_data=os.path.join('igblast','optional_file','human_gl.aux')
     constantdb=os.path.join('human_gl_C.fasta')
     blast_path="blastn"
     igblast_path="igblastn"
 elif(platform.system()=="Windows"):
-    IGDATA="\\be-svr-001-vm02-cos.charite.de\ag-pruess\Benutzer Ag Prüß\Momsen\software\internal_data"
-    auxiliary_data=os.path.join('ncbi-igblast-1.8.0','optional_file','human_gl.aux')
+    igblast_internal_data"\\be-svr-001-vm02-cos.charite.de\ag-pruess\Benutzer Ag Prüß\Momsen\software\internal_data"
+    igblast_auxiliary_data=os.path.join('ncbi-igblast-1.8.0','optional_file','human_gl.aux')
     constantdb=os.path.join('germline_c_db',"germline_c_db")
     blast_path=os.path.join('bin','blast-2.9.0+','bin','blastn.exe')
     igblast_path="bin\igblastn.exe"
