f7dea10735a3c3b88229f25f138cb1079218c0d1
automatedSequencing <35260345+automatedSequencing@users.noreply.github.com>
2020-08-20 19:30:13 +0200

    Update cBASE-testrun.sh

--- examples/cBASE-testrun.sh
+++ examples/cBASE-testrun.sh
@@ -1,2 +1,2 @@
 #/bin/bash
-./cBASE.py cBASE-test.xlsx cBASE-test-out.xlsx --dataprefix=SeqData/ --pcr2read A02:A03 --plasmidread C --shmanalysis D,E,F,G
+./cBASE.py cBASE-test.xlsx cBASE-example-run.xlsx --dataprefix=SeqData/ --pcr2read A02:A03 --plasmidread C --shmanalysis D,E,F,G
